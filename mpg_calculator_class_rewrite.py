b38be2106ca0724fabd861104f59b9a31fbf1c15
Jason Robinson <jmrobinsonma@gmail.com>
2019-06-07 20:47:00 -0400

    Rewrote original code as a class

--- mpg_calculator_class_rewrite.py
+++ mpg_calculator_class_rewrite.py
@@ -20,19 +20,18 @@ class Mpg:
 	def fuel_efficiency(self):
 		efficiency = self.miles / self.gallons
 		rounded_fuel_efficiency = round(efficiency, 2)
-		final_statement = print("\nYour fuel efficiency is {} Miles Per Gallon".format(rounded_fuel_efficiency))
+		final_statement = "\nYour fuel efficiency is {} Miles Per Gallon".format(rounded_fuel_efficiency)
 		return final_statement
 		
 	def fuel_cost_per_mile(self):
 		fuel_cost = self.price / (self.miles / self.gallons)
 		rounded_fuel_cost = round(fuel_cost, 2)
-		final_statement0 = print("Your fuel cost is ${} per mile".format(rounded_fuel_cost))
-		return final_statement0
+		final_statement = "\nYour fuel cost is ${} per mile".format(rounded_fuel_cost)
+		return final_statement
 		
 
 my_car = Mpg(user_price_per_gallon, user_gallons_purchased, user_miles_travelled)	
 
-print (Mpg.fuel_cost_per_mile(my_car))
 print(my_car.fuel_efficiency())
-
+print (Mpg.fuel_cost_per_mile(my_car))
 
